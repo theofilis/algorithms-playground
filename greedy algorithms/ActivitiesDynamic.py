d393a62e2625f794920f83af3e8f6c3199d711f4
theofilis <theofilis.g@gmail.com>
2013-08-13 16:42:28 +0300

    update Activities Dynamic

--- greedy algorithms/ActivitiesDynamic.py
+++ greedy algorithms/ActivitiesDynamic.py
@@ -2,16 +2,16 @@
 # -*- coding: UTF8 -*-
 
 def activities(s, f):
-	c = {}
+	n = len(s)
+	A = [0]
+	i = 0
 	
-	def init():
-		for i in range(len(s), 0, -1):
-			for j in range(i+1):
-				c[i,j] = 0;
-			
-		return c;
+	for m in range(1, n):
+		if s[m] >= f[i] :
+			A += [m]
+			i = m
 	
-	return init();
+	return A;
 
 s = [1,3,0,5,3,5, 6, 8, 8, 2,12]
 f = [4,5,6,7,8,9,10,11,12,13,14]
